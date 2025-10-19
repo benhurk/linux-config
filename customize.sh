2a744fb3ff23499b7ac7027580b6789aa76c2b52
benhur <benhurk2801@gmail.com>
2025-10-19 15:11:59 -0300

    Go back

--- customize.sh
+++ customize.sh
@@ -9,35 +9,22 @@ if [ ! -d "$HOME/linux-config" ]; then
   git clone https://github.com/benhurk/linux-config.git "$HOME/linux-config"
 fi
 
-mkdir -p ~/.config
-mkdir -p ~/.local/share/fonts
-mkdir -p ~/.local/share/icons
-mkdir -p ~/.icons
-mkdir -p ~/Imagens/wallpapers
+rm -rf ~/.config/i3
+ln -s ~/linux-config/config/i3 ~/.config/i3
+rm -rf ~/.config/picom
+ln -s ~/linux-config/config/picom ~/.config/picom
+rm -rf ~/.config/polybar
+ln -s ~/linux-config/config/polybar ~/.config/polybar
+rm -rf ~/.config/rofi
+ln -s ~/linux-config/config/rofi ~/.config/rofi
+rm -rf ~/.config/kitty
+ln -s ~/linux-config/config/kitty ~/.config/kitty
+rm -f ~/.zshrc
+ln -s ~/linux-config/config/.zshrc ~/.zshrc
 
-link() {
-  src="$1"
-  dest="$2"
-  if [ -L "$dest" ] || [ -e "$dest" ]; then
-    rm -rf "$dest"
-  fi
-  ln -s "$src" "$dest"
-}
-
-# Config links
-link ~/linux-config/config/i3 ~/.config/i3
-link ~/linux-config/config/picom ~/.config/picom
-link ~/linux-config/config/polybar ~/.config/polybar
-link ~/linux-config/config/rofi ~/.config/rofi
-link ~/linux-config/config/kitty ~/.config/kitty
-link ~/linux-config/config/.zshrc ~/.zshrc
-link ~/linux-config/theme/mouse-cursors ~/.icons
-link ~/linux-config/theme/icons ~/.local/share/icons
-link ~/linux-config/theme/wallpapers ~/Imagens/wallpapers
-link ~/linux-config/fonts ~/.local/share/fonts
-
-fc-cache -fv
-
-log "Applying theme..."
+ln -s ~/linux-config/fonts ~/.local/share/fonts
+ln -s ~/linux-config/theme/mouse-cursors ~/.icons
+ln -s ~/linux-config/theme/icons ~/.local/share/icons
+ln -s ~/linux-config/theme/wallpapers ~/Imagens/wallpapers
 
 log "Done! Refresh i3 with Super + Shift + R."
