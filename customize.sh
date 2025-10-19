f4420e90cf006a5a2e78d7bf4d123a2c07b9c788
benhur <benhurk2801@gmail.com>
2025-10-19 15:32:49 -0300

    Cleaning linking commands

--- customize.sh
+++ customize.sh
@@ -9,22 +9,15 @@ if [ ! -d "$HOME/linux-config" ]; then
   git clone https://github.com/benhurk/linux-config.git "$HOME/linux-config"
 fi
 
-rm -rf ~/.config/i3
-ln -s ~/linux-config/config/i3 ~/.config/i3
-rm -rf ~/.config/picom
-ln -s ~/linux-config/config/picom ~/.config/picom
-rm -rf ~/.config/polybar
-ln -s ~/linux-config/config/polybar ~/.config/polybar
-rm -rf ~/.config/rofi
-ln -s ~/linux-config/config/rofi ~/.config/rofi
-rm -rf ~/.config/kitty
-ln -s ~/linux-config/config/kitty ~/.config/kitty
-rm -f ~/.zshrc
-ln -s ~/linux-config/config/.zshrc ~/.zshrc
+ln -sf ~/linux-config/config/i3 ~/.config/i3
+ln -sf ~/linux-config/config/picom ~/.config/picom
+ln -sf ~/linux-config/config/polybar ~/.config/polybar
+ln -sf ~/linux-config/config/rofi ~/.config/rofi
+ln -sf ~/linux-config/config/kitty ~/.config/kitty
+ln -sf ~/linux-config/config/.zshrc ~/.zshrc
+ln -sf ~/linux-config/fonts ~/.local/share/fonts
+ln -sf ~/linux-config/theme/mouse-cursors ~/.icons
+ln -sf ~/linux-config/theme/icons ~/.local/share/icons
+ln -sf ~/linux-config/theme/wallpapers ~/Imagens/wallpapers
 
-ln -s ~/linux-config/fonts ~/.local/share/fonts
-ln -s ~/linux-config/theme/mouse-cursors ~/.icons
-ln -s ~/linux-config/theme/icons ~/.local/share/icons
-ln -s ~/linux-config/theme/wallpapers ~/Imagens/wallpapers
-
-log "Done! Refresh i3 with Super + Shift + R."
+log "Done! Refresh i3 with Super + Shift + R and reopen applications."
