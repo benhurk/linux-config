aaf409da527b1410f8b70dfe9e1aa45aacf3fcad
benhur <benhurk2801@gmail.com>
2025-10-27 19:19:18 -0300

    Some polybar restyling

--- config/polybar/scripts/bluetooth_status.sh
+++ config/polybar/scripts/bluetooth_status.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 COLOR_PRIMARY=#61afef
+COLOR_SECONDARY=#ffb86c
 COLOR_ACTIVE=#90bf85
 COLOR_DISABLED=#abb2bf
 
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -l) -eq 0 ]; then
-  echo "%{F$COLOR_DISABLED}%{T3}󰂲%{T-} Off%{F-}"
+  echo "%{F$COLOR_SECONDARY}%{T3}󰂲%{T-}%{F-} %{F$COLOR_DISABLED}Off%{F-}"
 else
   if [ $(bluetoothctl devices Connected | wc -l) -gt 0 ]; then
     echo "%{F$COLOR_PRIMARY}%{T3}󰂱%{T-}%{F-} %{F$COLOR_ACTIVE}Conectado%{F-}"
