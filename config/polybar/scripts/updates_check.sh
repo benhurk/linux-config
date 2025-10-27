47d565b0c1150fdc76ce20df7c4b43bf4af3be93
benhur <benhurk2801@gmail.com>
2025-10-27 18:24:13 -0300

    Restyled polybar update module

--- config/polybar/scripts/updates_check.sh
+++ config/polybar/scripts/updates_check.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-COLOR_PRIMARY="#61afef"
-COLOR_DISABLED="#abb2bf"
-
 apt_updates=$(apt list --upgradable 2>/dev/null | grep -c upgradable)
 flatpak_updates=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
-total=$((apt_updates + flatpak_updates))
 
-if [ $total -gt 0 ]; then
-  echo "%{F$COLOR_PRIMARY T3}󰏔%{T-} $total%{F-}"
-else
-  echo "%{F$COLOR_DISABLED T3}󰏔%{T-} $total%{F-}"
-fi
+echo $((apt_updates + flatpak_updates))
