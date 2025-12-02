86d65b9af8ef15a1bcc4dee3eb7fdcc15bf9592f
benhur <benhurk2801@gmail.com>
2025-12-02 20:43:00 -0300

    Added prompting and improved updates script

--- scripts/polybar/updates_check.sh
+++ scripts/polybar/updates_check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-apt_updates=$(apt list --upgradable 2>/dev/null | grep -c "atualizável\|upgradable")
-flatpak_updates=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
-total=$((apt_updates + flatpak_updates))
+APT_UPDATES=$(apt list --upgradable 2>/dev/null | grep -c "atualizável\|upgradable")
+FLATPAK_UPDATES=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
+total=$((APT_UPDATES + FLATPAK_UPDATES))
 
 if [ $total -gt 0 ]; then
   echo "$total"
--- scripts/updates_list.sh
+++ scripts/updates_list.sh
@@ -4,8 +4,43 @@ set -e
 
 log() { echo -e "\033[1;32m[$1]\033[0m"; }
 
+APT_UPDATES=$(apt list --upgradable)
+FLATPAK_UPDATES=$(flatpak remote-ls --updates)
+APT_TOTAL=$(printf "%s" "$APT_UPDATES" 2>/dev/null | grep -c "atualizável\|upgradable")
+FLATPAK_TOTAL=$($FLATPAK_UPDATES 2>/dev/null | wc -l)
+
 log "apt"
-apt list --upgradable
+if [ $APT_TOTAL -gt 0 ]; then
+  echo -e "$APT_UPDATES\n"
+else
+  echo -e "0\n"
+fi
 
 log "flatpak"
-flatpak remote-ls --updates
+if [ $FLATPAK_TOTAL -gt 0 ]; then
+  echo -e "$FLATPAK_UPDATES\n"
+else
+  echo -e "0\n"
+fi
+
+update() {
+  if [ $APT_TOTAL -gt 0 ]; then
+    sudo apt update && sudo apt upgrade -y
+  fi
+
+  if [ $FLATPAK_TOTAL -gt 0 ]; then
+    flatpak update -y
+  fi
+
+  polybar-msg cmd restart >/dev/null
+}
+
+if [ $((APT_TOTAL + FLATPAK_TOTAL)) -gt 0 ]; then
+  read -p "Atualizar? (y/n) " choice
+
+  case "$choice" in
+  y | Y) update ;;
+  n | N) ;;
+  *) ;;
+  esac
+fi
