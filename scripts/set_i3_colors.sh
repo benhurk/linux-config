59baad06902dcdc727aa8024c7c18589756c225c
benhur <benhurk2801@gmail.com>
2025-12-14 11:45:30 -0300

    Use sed to update i3 colors

--- scripts/set_i3_colors.sh
+++ scripts/set_i3_colors.sh
@@ -2,22 +2,15 @@
 
 source "$HOME/scripts/env.sh"
 
-cat >~/.config/i3/colors.conf <<EOF
-set \$color_background $COLOR_BACKGROUND
-set \$color_background_alt $COLOR_BACKGROUND_ALT
-set \$color_foreground $COLOR_FOREGROUND
-set \$color_primary $COLOR_PRIMARY
-set \$color_secondary $COLOR_SECONDARY
-set \$color_disabled $COLOR_DISABLED
-set \$color_active $COLOR_ACTIVE
-set \$color_alert $COLOR_ALERT
+CONF_FILE="$HOME/.config/i3/colors.conf"
 
-client.focused          #ffffff #000000 #ffffff \$color_primary   \$color_primary
-client.focused_inactive #ffffff #000000 #ffffff \$color_secondary   \$color_secondary
-client.unfocused        #ffffff #000000 #ffffff \$color_disabled   \$color_disabled
-client.urgent           #ffffff #000000 #ffffff \$color_alert   \$color_alert
-client.placeholder      #000000 #000000 #ffffff \$color_secondary   \$color_secondary
-client.background               #ffffff
-EOF
+sed -i "s/^\(set \$color_background \)#[0-9a-fA-F]\{6\}/\1$COLOR_BACKGROUND/" $CONF_FILE
+sed -i "s/^\(set \$color_background_alt \)#[0-9a-fA-F]\{6\}/\1$COLOR_BACKGROUND_ALT/" $CONF_FILE
+sed -i "s/^\(set \$color_foreground \)#[0-9a-fA-F]\{6\}/\1$COLOR_FOREGROUND/" $CONF_FILE
+sed -i "s/^\(set \$color_primary \)#[0-9a-fA-F]\{6\}/\1$COLOR_PRIMARY/" $CONF_FILE
+sed -i "s/^\(set \$color_secondary \)#[0-9a-fA-F]\{6\}/\1$COLOR_SECONDARY/" $CONF_FILE
+sed -i "s/^\(set \$color_disabled \)#[0-9a-fA-F]\{6\}/\1$COLOR_DISABLED/" $CONF_FILE
+sed -i "s/^\(set \$color_active \)#[0-9a-fA-F]\{6\}/\1$COLOR_ACTIVE/" $CONF_FILE
+sed -i "s/^\(set \$color_alert \)#[0-9a-fA-F]\{6\}/\1$COLOR_ALERT/" $CONF_FILE
 
 i3-msg reload
