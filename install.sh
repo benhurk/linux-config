a2d1617f315d412c0447eaed13eab23b637ab736
benhur <benhurk2801@gmail.com>
2025-12-14 11:47:18 -0300

    Added new packages and Nix to the install script

--- install.sh
+++ install.sh
@@ -1,31 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
-
 log() { echo -e "\033[1;32m[INFO]\033[0m $1"; }
-
-log "Enabling flatpak..."
-sudo apt install -y flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 log "Installing core..."
 sudo apt install -y \
   xorg xinput mesa-utils \
   network-manager bluez pipewire-audio libnotify-bin \
-  zsh build-essential curl git gh unzip pipx fastfetch \
-  kitty i3-wm picom polybar rofi feh lightdm slick-greeter dunst xsct
-
-echo "Installing Neovim (AppImage)..."
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
-chmod u+x nvim-linux-x86_64.appimage
-sudo mkdir -p /opt/nvim
-sudo mv nvim-linux-x86_64.appimage /opt/nvim/nvim
-echo "export PATH="$PATH:/opt/nvim/"" >>~/.zshrc
-echo "export PATH="$PATH:/opt/nvim/"" >>~/.bashrc
+  zsh build-essential curl vim gh unzip pipx fastfetch \
+  kitty i3-wm picom polybar rofi feh lightdm slick-greeter dunst xsct flameshot
 
 sudo systemctl enable NetworkManager
 sudo systemctl enable bluetooth
 sudo systemctl enable lightdm
+
+log "Enabling flatpak..."
+sudo apt install -y flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+log "Running Nix installer..."
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 
 log "Changing shell..."
 chsh -s /usr/bin/zsh
