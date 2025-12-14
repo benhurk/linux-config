76782fd815fc5ffce801eba57a5afbc9857c3593
benhur <benhurk2801@gmail.com>
2025-12-14 11:57:00 -0300

    Fixed bugs and renamed updates script

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
-set -e
-
 log() { echo -e "\033[1;32m[$1]\033[0m"; }
 
-APT_UPDATES=$(apt list --upgradable)
+APT_UPDATES=$(apt list --upgradable | sed 1d)
 FLATPAK_UPDATES=$(flatpak remote-ls --updates)
-APT_TOTAL=$(printf "%s" "$APT_UPDATES" 2>/dev/null | grep -c "atualizável\|upgradable")
-FLATPAK_TOTAL=$($FLATPAK_UPDATES 2>/dev/null | wc -l)
+APT_TOTAL=$(apt list --upgradable 2>/dev/null | sed 1d | wc -l)
+FLATPAK_TOTAL=$(flatpak remote-ls --updates 2>/dev/null | wc -l)
 
 log "apt"
 if [ $APT_TOTAL -gt 0 ]; then
   echo -e "$APT_UPDATES\n"
 else
-  echo -e "0\n"
+  echo -e "Nenhuma atualização disponível\n"
 fi
 
 log "flatpak"
 if [ $FLATPAK_TOTAL -gt 0 ]; then
   echo -e "$FLATPAK_UPDATES\n"
 else
-  echo -e "0\n"
+  echo -e "Nenhuma atualização disponível\n"
 fi
 
 update() {
@@ -28,7 +26,7 @@ update() {
     sudo apt update && sudo apt upgrade -y
   fi
 
-  if [ $FLATPAK_TOTAL -gt 0 ]; then
+  if [ $FLATPAK_TOTAL != 0 ]; then
     flatpak update -y
   fi
 
@@ -36,7 +34,7 @@ update() {
 }
 
 if [ $((APT_TOTAL + FLATPAK_TOTAL)) -gt 0 ]; then
-  read -p "Atualizar? (y/n) " choice
+  read -p "Instalar atualizações? (y/n) " choice
 
   case "$choice" in
   y | Y) update ;;
