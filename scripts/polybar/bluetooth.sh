1e92de9ba9e31c1b3788768ce3914ec57c98b57b
benhur <benhurk2801@gmail.com>
2025-12-18 15:08:39 -0300

    Added text labels to the Polybar bluetooth module

--- scripts/polybar/bluetooth.sh
+++ scripts/polybar/bluetooth.sh
@@ -3,9 +3,9 @@
 source "$HOME/scripts/polybar/env.sh"
 
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -l) -eq 0 ]; then
-  echo "%{F$COLOR_DISABLED} Off%{F-}"
+  echo "%{F$COLOR_DISABLED}%{T2}%{T-} %{T1}Off%{T-}%{F-}"
 elif [ $(bluetoothctl devices Connected | wc -l) -gt 0 ]; then
-  echo "%{F$COLOR_ACTIVE} Conectado%{F-}"
+  echo "%{F$COLOR_ACTIVE}%{T2}%{T-} %{T1}Conectado%{T-}%{F-}"
 else
-  echo "%{F$COLOR_PRIMARY} On%{F-}"
+  echo "%{F$COLOR_PRIMARY}%{T2}%{T-} %{T1}On%{T-}%{F-}"
 fi
