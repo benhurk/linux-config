08113ced9f705b10240b41061cefe8d9248985d9
benhur <benhurk2801@gmail.com>
2025-12-26 18:39:48 -0300

    Tweaked catppuccin colors

--- themes/catppuccin/set_folders.sh
+++ themes/catppuccin/set_folders.sh
@@ -2,15 +2,8 @@
 
 VARIANT=$1
 
-if [ -z "$VARIANT" ]; then
-  echo "Usage: $0 <variant>"
-  echo "Variants: dark light"
-  echo "Example: $0 dark"
-  exit 1
-fi
-
 if [ $VARIANT == "dark" ]; then
-  papirus-folders --color cat-macchiato-lavender >/dev/null
+  papirus-folders --color cat-macchiato-mauve >/dev/null
 elif [ $VARIANT == "light" ]; then
   papirus-folders --color cat-latte-lavender >/dev/null
 else
