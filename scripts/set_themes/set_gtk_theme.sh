dddad69981c9bd79fa03838f1010e4c3d507016b
benhur <benhurk2801@gmail.com>
2025-12-27 14:44:20 -0300

    Added cursor and folder theme changes

--- scripts/set_themes/set_gtk_theme.sh
+++ scripts/set_themes/set_gtk_theme.sh
@@ -24,14 +24,18 @@ if [ ! -d $THEMEDIR ]; then
   exit 1
 fi
 
-if [ $VARIANT == "dark" ]; then
-  cp "$THEMEDIR/gtk-dark.css" "$HOME/.config/gtk-3.0/colorscheme.css"
-  cp "$THEMEDIR/gtk-dark.css" "$HOME/.config/gtk-4.0/colorscheme.css"
-  $THEMEDIR/set_folders.sh dark
-elif [ $VARIANT == "light" ]; then
-  cp "$THEMEDIR/gtk-light.css" "$HOME/.config/gtk-3.0/colorscheme.css"
-  cp "$THEMEDIR/gtk-light.css" "$HOME/.config/gtk-4.0/colorscheme.css"
-  $THEMEDIR/set_folders.sh light
+if [ $VARIANT == "dark" ] || [ $VARIANT == "light" ]; then
+  cp "$THEMEDIR/gtk-$VARIANT.css" "$HOME/.config/gtk-3.0/colorscheme.css"
+  cp "$THEMEDIR/gtk-$VARIANT.css" "$HOME/.config/gtk-4.0/colorscheme.css"
+
+  FOLDERS=$(cat $THEMEDIR/extras.ini | grep "folders-$VARIANT" | awk -F '=' '{print $2}')
+  papirus-folders --color $FOLDERS >/dev/null
+
+  CURSOR=$(cat $THEMEDIR/extras.ini | grep "mouse-cursor" | awk -F '=' '{print $2}')
+  sed -i "s/^gtk-cursor-theme-name=.*/gtk-cursor-theme-name=$CURSOR/" $HOME/.config/gtk-3.0/settings.ini
+  sed -i "s/^gtk-cursor-theme-name=.*/gtk-cursor-theme-name=\"$CURSOR\"/" $HOME/.gtkrc-2.0
+  sed -i "s/^Inherits=.*/Inherits=$CURSOR/" $HOME/.icons/default/index.theme
+  sed -i "s/^Xcursor.theme: .*/Xcursor.theme: $CURSOR/" $HOME/.Xresources
 else
   echo "Invalid variant. Available: dark or light"
   exit 1
