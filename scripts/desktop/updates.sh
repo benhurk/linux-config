d73f952a56c028703406df3011dc17255fd506ea
benhurk <benhurk2801@proton.me>
2026-02-18 18:22:08 -0300

    Rewrote updates script

--- scripts/desktop/updates.sh
+++ scripts/desktop/updates.sh
@@ -1,42 +1,55 @@
 #!/bin/bash
 
-log() { echo -e "\033[1;32m[$1]\033[0m"; }
+log() { echo -e "\033[1;32m$1\033[0m"; }
 
-apt_updates=$(apt list --upgradable | grep "atualizável\|upgradable")
+apt_updates=$(apt list --upgradable 2>/dev/null | grep "atualizável\|upgradable")
 flatpak_updates=$(flatpak remote-ls --updates)
 
-log " apt"
+log "[  apt ]"
 if [ ! -z "$apt_updates" ]; then
   echo -e "$apt_updates\n"
 else
   echo -e "Nenhuma atualização disponível\n"
 fi
 
-log " Flatpak"
+log "[  Flatpak ]"
 if [ ! -z "$flatpak_updates" ]; then
   echo -e "$flatpak_updates\n"
 else
   echo -e "Nenhuma atualização disponível\n"
 fi
 
-update() {
+update_apt() {
   if [ ! -z "$apt_updates" ]; then
-    sudo apt update && sudo apt upgrade -y
+    sudo apt upgrade -y
   fi
+}
 
+update_flatpak() {
   if [ ! -z "$flatpak_updates" ]; then
     flatpak update -y
   fi
+}
 
-  pkill -RTMIN+1 waybar
+update_all() {
+  update_apt
+  update_flatpak
 }
 
 if [ ! -z "$apt_updates" ] || [ ! -z "$flatpak_updates" ]; then
-  read -p "Instalar atualizações? (y/n) " choice
+  log "######\n"
+
+  echo "Opções:"
+  echo -e "[1] Tudo | [2] apt | [3] Flatpak | [0] Cancelar\n"
+
+  read -p "Atualizar: " choice
 
   case "$choice" in
-  y | Y) update ;;
-  n | N) ;;
-  *) ;;
+  "1") update_all ;;
+  "2") update_apt ;;
+  "3") update_flatpak ;;
+  "0" | *) exit ;;
   esac
 fi
+
+pkill -RTMIN+1 waybar
