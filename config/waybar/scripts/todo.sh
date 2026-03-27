c2d2151a3eff75e9e60cfb3583ee7d8c0f7a5095
benhurk <benhurk2801@proton.me>
2026-03-27 19:49:45 -0300

    bar: Improved todo module, changed tooltip format and click actions

--- config/waybar/scripts/todo.sh
+++ config/waybar/scripts/todo.sh
@@ -6,7 +6,8 @@ if ! todo &>/dev/null; then
 fi
 
 current_task=$(todo current)
+rest=$(todo ls --no-index | sed -e "/^$current_task$/d" -e "s/^/• /" | tr '\n' '\r')
 
 if [ -n "$current_task" ]; then
-    echo "{ \"text\": \"$current_task\" }"
+    echo "{ \"text\": \"$current_task\", \"alt\": \"${rest}\" }"
 fi
