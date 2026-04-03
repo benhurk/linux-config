a8263e7a9e5906694841b76690bf87443b937720
benhurk <benhurk2801@proton.me>
2026-04-03 15:13:47 -0400

    rofi: Dynamically gets icon theme for the launcher menu

--- config/rofi/launcher/launcher.sh
+++ config/rofi/launcher/launcher.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 dir="$HOME/.config/rofi/launcher"
+icon_theme=$(gsettings get org.gnome.desktop.interface icon-theme)
 
 rofi \
-  -show drun \
-  -theme ${dir}/style.rasi
+    -show drun \
+    -theme ${dir}/style.rasi \
+    -icon-theme "${icon_theme//\'}"
