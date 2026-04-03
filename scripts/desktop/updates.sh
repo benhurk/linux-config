8134b4fdb16bcee3b04b11df793fd30530391317
benhurk <benhurk2801@proton.me>
2026-04-03 15:14:48 -0400

    scripts: Improved update manager formatting

--- scripts/desktop/updates.sh
+++ scripts/desktop/updates.sh
@@ -7,49 +7,47 @@ flatpak_updates=$(flatpak remote-ls --updates)
 
 log "[  apt ]"
 if [ ! -z "$apt_updates" ]; then
-  echo -e "$apt_updates\n"
+    echo -e "$apt_updates\n"
 else
-  echo -e "Nenhuma atualização disponível\n"
+    echo -e "Nenhuma atualização disponível\n"
 fi
 
 log "[  Flatpak ]"
 if [ ! -z "$flatpak_updates" ]; then
-  echo -e "$flatpak_updates\n"
+    echo -e "$flatpak_updates\n"
 else
-  echo -e "Nenhuma atualização disponível\n"
+    echo -e "Nenhuma atualização disponível\n"
 fi
 
 update_apt() {
-  if [ ! -z "$apt_updates" ]; then
-    sudo apt upgrade -y
-  fi
+    if [ ! -z "$apt_updates" ]; then
+        sudo apt upgrade -y
+    fi
 }
 
 update_flatpak() {
-  if [ ! -z "$flatpak_updates" ]; then
-    flatpak update -y
-  fi
+    if [ ! -z "$flatpak_updates" ]; then
+        flatpak update -y
+    fi
 }
 
 update_all() {
-  update_apt
-  update_flatpak
+    update_apt
+    update_flatpak
 }
 
 if [ ! -z "$apt_updates" ] || [ ! -z "$flatpak_updates" ]; then
-  log "######\n"
+    log "Atualizar:"
+    echo -e "[1] Tudo | [2] apt | [3] Flatpak | [0] Cancelar\n"
 
-  echo "Opções:"
-  echo -e "[1] Tudo | [2] apt | [3] Flatpak | [0] Cancelar\n"
+    read -p "Opção: " choice
 
-  read -p "Atualizar: " choice
-
-  case "$choice" in
-  "1") update_all ;;
-  "2") update_apt ;;
-  "3") update_flatpak ;;
-  "0" | *) exit ;;
-  esac
+    case "$choice" in
+        "1") update_all ;;
+        "2") update_apt ;;
+        "3") update_flatpak ;;
+        "0" | *) exit ;;
+    esac
 fi
 
 pkill -RTMIN+1 waybar
